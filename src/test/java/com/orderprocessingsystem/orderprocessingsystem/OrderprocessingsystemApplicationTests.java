01bb4d726fae12ffae116d5e431f9e271f5d1320
mihkel.koks <mihkel.koks@eek.ee>
2024-11-11 15:34:50 +0200

    first commit

---
+++ src/test/java/com/orderprocessingsystem/orderprocessingsystem/OrderprocessingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderprocessingsystem.orderprocessingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderprocessingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
